19b3ebaeffa0f9bcbb0f1dfc35904abd3c947f6d
luzkrn <luzkarinamollojanco@gmail.com>
2026-03-01 18:51:48 -0400

    tareaa

--- Ejercicio01/ejercicio01.py
+++ Ejercicio01/ejercicio01.py
@@ -1,1 +1,14 @@
-print("chufvhilio")
+class Anime:
+    def __init__(self,nombre,genero,nroEpisodios,):
+        self.nombre=nombre
+        self.genero=genero
+        self.__nroEpisodios=nroEpisodios
+        self.__episodios=[]
+
+    def __str__(self):
+        return f"anime: {self.nombre} , genero: {self.genero} , episodios: {self.__nroEpisodios}"
+class Main:
+        
+        A = Anime("Gachiakuta", "Accion", 24)
+        print(A)
+#recomendado buen anime y ya se confirmo 2 temporada (づ｡◕‿‿◕｡)づ
